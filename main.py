9f677f8a2322b6144b5efba58e70c0e9b0b49e47
Akhil <duvvaakhil.0@gmail.com>
2023-08-12 17:11:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ import discord
 from moderation import Moderation
 from normal import Normal
 from exception_handler import Error_handler
+from background_functions import *
 
 import os 
 token = os.environ['token']
 
-WELCOME_CHANNEL = {Channel name}
-VERIFICATION_CHANNEL = {Channel name}
-BOT-COMMANDS_CHANNEL = {Channel name}
+
 intents = disnake.Intents.all()
 bot = commands.InteractionBot(intents=intents)
 
@@ -29,14 +28,7 @@ async def on_ready():
     print(f"Bot started with the ID: {bot.user}")
     
 
-# Automatic running events
-@bot.event
-async def on_member_join(member):
-    welcome_channel = discord.utils.get(member.guild.channels, name=WELCOME_CHANNEL)
-    verification_channel = discord.utils.get(member.guild.channels, name=VERIFICATION_CHANNEL)
-    bot-commands_channel = discord.utils.get(member.guild.channels, name=BOT-COMMANDS_CHANNEL)
-    await welcome_channel.send(f"Welcome {member.mention}! Please read our rules and verify yourself at <#{verification_channel}> to access the server. Call me with a simple command /help in <#{bot-commands_channel}> channel. Have a great time!!")
-    
+
 
 if __name__ == "__main__":
     bot.run(token)
