13eacebadc20751f6b22266fac4b7ab7485e719f
Akhil <duvvaakhil.0@gmail.com>
2023-08-12 16:53:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,16 +33,6 @@ async def on_member_join(member):
     welcome_channel = discord.utils.get(member.guild.channels, name=WELCOME_CHANNEL)
     await welcome_channel.send(f"Welcome {member.mention}! Please read our rules and verify yourself at <#1130116826114826242> to access the server. Call me with a simple command ?ahelp in <#1130116826114826246> channel. Have a great time!!")
 
-async def send_message_in_private_channel():
-    await bot.wait_until_ready()
-    private_channel_id = 123467890123456789    # Change with your Reporti  
-    
-    while not bot.is_closed():
-        private_channel = bot.get_channel(private_channel_id)
-        latency = bot.latency
-        now = time.localtime()
-        await private_channel.send(f"Reporting Roger!! The time is {now.tm_hour}:{now.tm_min}:{now.tm_sec} now. Latency: {round(latency * 1000)}ms")
-        await asyncio.sleep(86400)# Sleep for 24 hours (86400 seconds)
 
 if __name__ == "__main__":
     bot.run(token)
